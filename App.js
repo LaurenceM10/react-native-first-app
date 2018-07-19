f8d9c3eff5f1d7099487fb6c0071cffaeeb8f4e1
Lauren <laurenmontenegro10@gmail.com>
2018-07-19 01:12:10 -0600

    Testing LinearGradient and Alerts

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class App extends React.Component {
+
+  static onButtonClick(){
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      { cancelable: false }
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.textStyle}>Hello World</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <LinearGradient
+          colors={['red', 'orange']}
+          start={{x: 0.0, y: 1.0}}
+          end={{x: 1.0, y: 1.0}}
+          style={{ padding: 15, alignItems: 'center', borderRadius: 5 }}>
+          <Text
+            style={{
+              backgroundColor: 'transparent',
+              fontSize: 15,
+              color: '#fff',
+            }}
+            onClick={this.onButtonClick}
+            >
+            Sign in with Facebook
+          </Text>
+        </LinearGradient>
       </View>
     );
   }
